54e8ecc2a92832812bb44386dce5f7158ba78e58
bang-a-coder <nikitas4717@gmail.com>
2021-03-24 13:05:52 +0100

    change slide functionality

--- src/onboarding/popUp.js
+++ src/onboarding/popUp.js
@@ -9,15 +9,20 @@ export class popUp extends Pragma{
         this.background 
         this.popUp
         this.popUpContent
+        this.nextBtn
     }
 
     render(){
         this.background = _e('div.blurred-bg').appendTo('body')
 
-        this.popUp = _e('div.popUp').appendTo(this.background)
+        this.popUp = _p('popUpPragma')
+                    .as(_e('div.popUp'))
+                    .appendTo(this.background)
+                    
 
-        let nextBtn = _e('div.next-btn')
+        this.nextBtn = _e('div.next-btn')
                       .html(`<div class="next-icon">${icons['back-icon']}</div>`)
+                      
                       .appendTo(this.popUp)
 
         this.popUpContent = _e('div.popUpContent').appendTo(this.popUp)
--- src/onboarding/popUpOb.js
+++ src/onboarding/popUpOb.js
@@ -10,12 +10,48 @@ export class popUpOb extends popUp{
     }
 
     addContent() {
-        let speedBoat = _e('div.boat')
+        let slides = []
+        let spaceBoat = _e('div.boat')
                         .html(`
                         <h1 class="boat-title">Press the spacebar to start/stop the pointer</h1>
                         ${icons['spacebar-3d']}
                         `)
                         .appendTo(this.popUpContent)
-                        
+            slides.push(spaceBoat)
+
+        let speedBoat = _e('div.boat')
+                        .html(`
+                        <h1 class="boat-title">Adjust the speed, by clicking or through your keyboard</h1>
+                        ${icons['speedBoat']}
+                        `)
+                        .appendTo(this.popUpContent)
+                        .addClass('displayN')
+            slides.push(speedBoat)
+
+        let clickBoat = _e('div.boat')
+                        .html(`
+                        <h1 class="boat-title">Adjust the speed, by clicking or through your keyboard</h1>
+                        ${icons['clickBoat']}
+                        `)
+                        .appendTo(this.popUpContent)
+                        .addClass('displayN')
+            slides.push(clickBoat)
+
+        this.popUp.adopt(...slides)
+                  .run(function(){
+                        console.log('CHILLLLDREEEEEN')
+                        console.log(this.children)
+                    })
+
+        this.popUp.value = 0
+
+        this.nextBtn.listenTo('click',()=>{
+                            this.popUp.value++
+                            this.popUp.children[this.popUp.value].toggleClass('displayN')
+                            this.popUp.children[this.popUp._lv].toggleClass('displayN')                            
+                        })            
+
+
+
     }
 }
