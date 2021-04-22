d266b7e18bd9c8b123709d7a6dad9e938b68800c
robo-monk <rrobomonk@gmail.com>
2021-04-22 05:16:57 +0200

    correct wegex

--- docs/scripts/lectorjs.umd.js
+++ docs/scripts/lectorjs.umd.js
@@ -16584,43 +16584,6 @@
     }
   }
 
-  // function wfyInner(desc){
-  //   if (!desc) return false
-  //   desc = _e(desc)
-  //   let txt = desc.textContent
-  //   if (txt.length === 0) return false
-
-  //   let inner = ""
-  //   for (let txt of desc.textContent.split(" ")){
-  //     // console.log(txt)
-  //     let noWhiteSpace = txt.replace(/\s/g, "")
-  //     inner += noWhiteSpace.length!=0 ? "<w>"+txt.split(" ").join("</w> <w>")+"</w> " : txt
-  //   }
-
-  //   desc.html(inner)
-  // }
-
-  // function wfyElement(element){
-  //   element = _e(element)
-  //   let nodes = element.findAll("*")
-  //   if (nodes.length == 0) return wfyInner(wfyInner(element))
-  //   nodes.forEach(desc => wfyElement(desc))
-  // }
-
-  // export function wfy(element){
-  //   // console.log(`wfying ${JSON.stringify(element)}`)
-  //   element = _e(element)
-  //   // if (element.textContent.replaceAll(" ", "").length<1) return false
-  //   let txtNodes = element.findAll("*")
-  //   if (txtNodes.length==0) return wfyElement(element)
-  //   // txtNodes.each((i, el) => {
-  //   //   wfy(el)
-  //   // })
-  //   txtNodes.forEach(el => wfy(el))
-  //   return true
-  // }
-
-
   let parser = new DOMParser();
 
   function escapeHtml(unsafe) {
@@ -16632,7 +16595,8 @@
     // .replace(/'/g, "&#039;");
   }
 
-  const wregex = /(\w+)/gm;
+  // const wregex = /(\w+)/gm
+  const wregex = /[^\s] +/gm;
   const obsKey = O.rk(5);
   const obs = {
     "<": `;;#${obsKey}0;`,
@@ -16655,7 +16619,7 @@
 
   function wegex(str) {
     // return str
-    return str.replaceAll(wregex, (match, re) => esc("<w>") + escapeHtml(re) + esc("</w> "))
+    return str.replaceAll(wregex, (match, re, i) => esc(`<w>`) + escapeHtml(re) + esc("</w>"))
   }
 
   function wfyInner(desc) {
--- src/helpers/wfy.js
+++ src/helpers/wfy.js
@@ -1,42 +1,5 @@
 import { _e, util } from 'pragmajs'
 
-// function wfyInner(desc){
-//   if (!desc) return false
-//   desc = _e(desc)
-//   let txt = desc.textContent
-//   if (txt.length === 0) return false
-
-//   let inner = ""
-//   for (let txt of desc.textContent.split(" ")){
-//     // console.log(txt)
-//     let noWhiteSpace = txt.replace(/\s/g, "")
-//     inner += noWhiteSpace.length!=0 ? "<w>"+txt.split(" ").join("</w> <w>")+"</w> " : txt
-//   }
-
-//   desc.html(inner)
-// }
-
-// function wfyElement(element){
-//   element = _e(element)
-//   let nodes = element.findAll("*")
-//   if (nodes.length == 0) return wfyInner(wfyInner(element))
-//   nodes.forEach(desc => wfyElement(desc))
-// }
-
-// export function wfy(element){
-//   // console.log(`wfying ${JSON.stringify(element)}`)
-//   element = _e(element)
-//   // if (element.textContent.replaceAll(" ", "").length<1) return false
-//   let txtNodes = element.findAll("*")
-//   if (txtNodes.length==0) return wfyElement(element)
-//   // txtNodes.each((i, el) => {
-//   //   wfy(el)
-//   // })
-//   txtNodes.forEach(el => wfy(el))
-//   return true
-// }
-
-
 let parser = new DOMParser()
 
 export function escapeHtml(unsafe) {
@@ -48,7 +11,8 @@ export function escapeHtml(unsafe) {
   // .replace(/'/g, "&#039;");
 }
 
-const wregex = /(\w+)/gm
+// const wregex = /(\w+)/gm
+const wregex = /[^\s] +/gm
 const obsKey = util.rk(5)
 const obs = {
   "<": `;;#${obsKey}0;`,
@@ -71,7 +35,7 @@ const unesc = (str) => {
 
 function wegex(str) {
   // return str
-  return str.replaceAll(wregex, (match, re) => esc("<w>") + escapeHtml(re) + esc("</w> "))
+  return str.replaceAll(wregex, (match, re, i) => esc(`<w>`) + escapeHtml(re) + esc("</w>"))
 }
 
 function wfyInner(desc) {
