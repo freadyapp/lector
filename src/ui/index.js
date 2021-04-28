d7fa47819f3067e78016a0ae9c4561210027d0d5
bang-a-coder <nikitas4717@gmail.com>
2021-04-28 12:10:19 +0200

    css fixed

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,5 +1,4 @@
-import LectorSettings from "./lectorSettings"
 import { Settings } from "./settings/settings"
 export { addSettingsToLector } from "./lectorSettings2"
 
-export { LectorSettings, Settings }
+export { Settings }
