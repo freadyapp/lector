0dd8532434cad9e9aad8c1c0120cdf8d9c240f3f
robo-monk <rrobomonk@gmail.com>
2021-06-08 03:01:58 +0200

    release 0.9.15

--- rollup.config.js
+++ rollup.config.js
@@ -1,52 +1,52 @@
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
-import { terser } from 'rollup-plugin-terser'
-import strip from "@rollup/plugin-strip"
+import { terser } from "rollup-plugin-terser";
+import strip from "@rollup/plugin-strip";
 
-import sizes from 'rollup-plugin-sizes'
-import json from '@rollup/plugin-json'
-import visualizer from 'rollup-plugin-visualizer'
-import pkg from './package.json'
-import { babel } from '@rollup/plugin-babel'
+import sizes from "rollup-plugin-sizes";
+import json from "@rollup/plugin-json";
+import visualizer from "rollup-plugin-visualizer";
+import pkg from "./package.json";
+import { babel } from "@rollup/plugin-babel";
 
-console.log('[ BUNDLING ] @ ', process.env['LECTOR_ENV'])
+console.log("[ BUNDLING ] @ ", process.env["LECTOR_ENV"]);
 
-const env = process.env['LECTOR_ENV']
-const prod = env == 'production' || env == 'prod'
+const env = process.env["LECTOR_ENV"];
+const prod = env == "production" || env == "prod";
 
 function ifProd(plug, params) {
-  return prod ? plug(params) : null
+  return prod ? plug(params) : null;
 }
 
 const plugs = [
   json(),
-  babel({ babelHelpers: 'bundled' }),
+  babel({ babelHelpers: "bundled" }),
   ifProd(visualizer, {
-    filename: 'docs/stats.html',
-    title: 'LectorJS Visualised',
+    filename: "docs/stats.html",
+    title: "LectorJS Visualised",
     sourcemap: false,
   }),
   ifProd(strip),
   ifProd(terser), // mini
   ifProd(sizes),
-]
+];
 
 export default [
   // browser-friendly UMD build
   {
-    input: 'src/index.js',
+    input: "src/index.js",
     //external: ['tippy', 'mousetrap', 'animejs' ],
     output: [
       {
-        name: 'lector',
+        name: "lector",
         file: pkg.browser,
-        format: 'umd',
+        format: "umd",
       },
       {
-        name: 'lector',
-        file: 'docs/scripts/lectorjs.umd.js',
-        format: 'umd',
+        name: "lector",
+        file: "docs/scripts/lectorjs.umd.js",
+        format: "umd",
       },
     ],
     plugins: [
@@ -55,13 +55,13 @@ export default [
     ].concat(plugs),
   },
   {
-    input: 'src/index.js',
-    external: ['ms', 'tippy', 'mousetrap', 'animejs'],
+    input: "src/index.js",
+    external: ["ms", "tippy", "mousetrap", "animejs"],
     // external: ['ms'],
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es', sourcemap: true },
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "es", sourcemap: true },
     ],
     plugins: [resolve()].concat(plugs),
   },
-]
+];
